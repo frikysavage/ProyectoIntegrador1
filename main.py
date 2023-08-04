55c15d6ba230d94ab24542d6734830784c9852fe
Juan Henriquez <juanxlcalisthenics@gmail.com>
2023-08-03 22:35:02 -0500

    parte 3 proyecto integrador

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-import readchar
-from readchar import readkey, key
-def Juego():
-    d=0
-    jueg=True
-    while d==0:
-     print("Hola y bienvenido al juego deL Laberinto por favor escribe tu nombre para iniciar el juego o presiona 1 para salir")
-     nombre=input()
-     if nombre=="1":
-      d=1
-     else:
-        print("Escribe lo que que quieras ", nombre)
-        while jueg==True:
-          tecla = readchar.readkey()
-          teclaespecial= readkey()
-          print(tecla, end="")
-          if teclaespecial == key.UP:
-             print("Gracias por jugar")
-             break
-
+import os, readchar
+from readchar import readkey
+def borrarpantalla():
+    contador=0
+    print("Bienvenido al contador de borradas , escribe lo que quieras para contar y borrar")
+    while contador<50:
+        tecla=readchar.readkey()
+        if ord(tecla)>0:
+            os.system('cls' if os.name=='nt' else 'clear')
+            contador=contador+1
+            print(contador)
+        
+    
 if __name__ == "__main__":
-    Juego()
+    borrarpantalla()
